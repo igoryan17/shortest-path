a20d339fc5b36b019fbe91826d854cf25afee92f
Igor Yadrov <i.yadrov@brain4net.com>
2019-02-17 22:46:35 +0300

    success compilation

--- src/main/java/com/igoryan/services/GraphService.java
+++ src/main/java/com/igoryan/services/GraphService.java
@@ -1,17 +1,5 @@
 package com.igoryan.services;
 
-import it.uniroma1.dis.wsngroup.gexf4j.core.Edge;
-import it.uniroma1.dis.wsngroup.gexf4j.core.Graph;
-import it.uniroma1.dis.wsngroup.gexf4j.core.Node;
-import java.util.List;
-
 public interface GraphService {
 
-  void add(Graph graph);
-
-  void update(Edge edge, int weight);
-
-  int distance(Node x, Node y);
-
-  List<Node> path(Node x, Node y);
 }
